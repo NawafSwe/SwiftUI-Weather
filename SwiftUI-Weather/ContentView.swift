e470e8d7502f43370d79f0ef0599d5b1f3b36584
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-06 21:09:47 +0300

    creating first view contains the city name and image of temprature state and degree

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,8 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //making alignment .top to make the VStack on top of the background
+        ZStack {
+            //making a background color
+            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)), Color(#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1))]), startPoint: .bottomTrailing, endPoint: .topLeading)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Cupertino, CA")
+                    .font(.title)
+                    .font(.system(size:32))
+                    .foregroundColor(.white)
+                    .padding()
+                    Spacer()
+                
+                //VStack holds the temp State and the temp Degree
+                VStack{
+                    //temperature state
+                    Image(systemName: "cloud.bolt.rain")
+                        .resizable()
+                        .font(.system(size:20))
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 150, alignment: .center)
+                        .padding()
+                    
+                    //temperature degree
+                    Text("76C")
+                        .foregroundColor(.white)
+                        .font(.system(size: 50))
+                    Spacer()
+                }
+  
+                
+                
+                
+            }
+        }
+  
     }
 }
 
