6e405da2f4beeea0ba672a2c6835909a94ff2a6d
NawafSwe <nawaf.jr9@hotmail.com>
2020-11-06 04:39:13 +0300

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Nawaf B Al sharqi on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
